9dde799a37528ac52beff5b1e6634c281aa9860d
Abhishek Kumar <abhishekkumar@jmangroup.com>
2024-08-07 16:29:56 +0530

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors())
+
+
+
+app.get("/webhook", (req, res) => {
+    const body = req.body;
+    return res.json({ message: "Ok" })
+})
+
+app.listen(5000, () => console.log(PORT))
